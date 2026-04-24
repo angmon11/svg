5f16ec7a0278224c75e46b481c12e9a035524903
angmon11 <angmonscethe@gmail.com>
2026-04-24 10:25:48 +0200

    Laba 6, zad 1, ^_^

---
+++ genealogy/src/PersonWithParentString.java
@@ -0,0 +1,8 @@
+public class PersonWithParentString {
+    private Person person;
+    private String[] parents;
+
+    public PersonWithParentString(Person person, String[] parents){
+        this.person = person;
+    }
+}
