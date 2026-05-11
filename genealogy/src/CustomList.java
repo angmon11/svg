83ab5d59f237c81db5fc20872db89e104a368a37
angmon11 <angmonscethe@gmail.com>
2026-05-11 09:48:23 +0200

    absolute gavno

---
+++ genealogy/src/CustomList.java
@@ -0,0 +1,138 @@
+import java.util.AbstractList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
+public class CustomList<T> extends AbstractList<T> {
+    private Node<T> head = null;
+
+    private Node<T> tail = null;
+
+    public void addLast(T value){
+        Node<T> current = new Node<>(value, null);
+        if( head == null ){
+            head = current;
+        }else{
+            tail.next = current;
+        }
+        tail = current;
+    }
+
+    public T getLast(){
+        try{
+            return tail.value;
+        } catch (NullPointerException e){
+            throw new NoSuchElementException(e);
+        }
+    }
+    public void addFirst(T value){
+        Node<T> current = new Node<>(value, head);
+        head = current;
+        if(tail == null){
+            tail = current;
+        }
+    }
+
+    public T getFirst(){
+        try{
+            return head.value;
+        } catch (NullPointerException e){
+            throw new NoSuchElementException(e);
+        }
+    }
+
+    public T removeFirst(){
+        if(head == null){
+            throw new NoSuchElementException();
+        }
+        Node<T> current = head;
+        if(head == tail){
+            tail = null;
+        }
+        head = head.next;
+        return current.value;
+    }
+
+    public T removeLast(){
+        if(tail == null){
+            throw new NoSuchElementException();
+        }
+        Node<T> current = head;
+        if(head == tail){
+            head = null;
+            tail = null;
+            return current.value;
+        }else{
+            while (current.next != tail){
+                current = current.next;
+            }
+            T result = tail.value;
+            current.next = null;
+            tail = current;
+            return result;
+        }
+
+    }
+
+    @Override
+    public T get(int index) {
+        if(index ==0){
+            if(head==null)return null;
+            return head.value;
+        }else{
+            Node<T> current = head;
+            for(int i =0; i < index; i++){
+                if(current.next == null) return null;
+                current = current.next;
+            }
+            return current.value;
+        }
+
+    }
+
+    @Override
+    public int size() {
+        if(head==null){
+            return 0;
+        }else{
+            Node<T> current = head;
+            int i = 1;
+            while ( current.next != null){
+                current = current.next;
+                i++;
+            }
+            return i;
+        }
+    }
+
+    public boolean add(T t){
+        addLast(t);
+        return true;
+    }
+    public Iterator<T> iterator(){
+        return new Iterator<T>(){
+            private Node<T> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                T result = current.value;
+                current = current.next;
+                return result;
+            }
+
+        };
+    }
+
+    public Stream<T> stream() {
+        Stream.Builder<T> builder = Stream.builder();
+        for (T value : this) {
+            builder.accept(value);
+        }
+        return builder.build();
+    }
+}
--- genealogy/src/Main.java
+++ genealogy/src/Main.java
@@ -5,39 +5,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            List<Person> people = Person.fromCsv("family.csv");
+        CustomList<String> list = new CustomList<>();
 
-            List<Person> sortedDead = Person.getDeceasedByLifespan(people);
+        list.addLast("ala");
+        list.addLast("ma");
+        list.addLast("kota");
 
-            PlantUmlRunner.setJarPath("/home/student/Pobrane/plantuml-java8-SNAPSHOT.jar");
-            PlantUmlRunner.generate(Person.generateTree(
-                    people, text -> String.format("%s #FFFF00", text), sortedDead::contains), "output", "test");
-            //Person.toBinaryFile(people,"data.bin");
-            //List<Person> people2 = Person.fromBinaryFile("data.bin");
+        list.addFirst("Mala");
 
-            //List<Person> filtered = Person.filterPersonbySubstring(people, "ska");
-            //filtered.stream().map(Person::name).forEach(System.out::println);
 
-            //List<Person> sorted = Person.filterPersonbySubstring(people, "ska");
-            //sorted.stream().map(Person::name).forEach(System.out::println);
-            Person p = Person.getOldestLiving(people);
-            System.out.println(p);
-            //List<Person> sortedDead = Person.getDeceasedByLifespan(people);
-            //sortedDead.stream().map(Person::name).forEach(System.out::println);
-            //System.out.println(people);
-        } catch (IOException e){
-            System.err.println("Blad dostepu do pliku" + e.getMessage());
+        for(String s : list){
+            System.out.println(s);
         }
-//        PlantUmlRunner.setJarPath("/home/student/Pobrane/plantuml-java8-SNAPSHOT.jar");
-//        String umlData = "@startuml\n" +
-//                "Alice -> Bob: Authentication Request\n" +
-//                "Bob --> Alice: Authentication Response\n" +
-//                "\n" +
-//                "Alice -> Bob: Another authentication Request\n" +
-//                "Alice <-- Bob: Another authentication Response\n" +
-//                "@enduml";
-        //PlantUmlRunner.generate(, "output", "test");
+
+        list.stream()
+                .sorted()
+                .forEach(System.out::println);
 
     }
 }
